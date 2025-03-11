f444c836ec10b5acf2fb34cede02a7029ac9fa22
YogeshKR <yyogeshyogi634@gmail.com>
2025-03-11 15:51:47 +0530

    web design workshop

---
+++ workshop/form.js
@@ -0,0 +1,9 @@
+function validateForm(){
+    alert('Gender')
+    let SelectGender=document.querySelector('input[name=Gender]:checked')
+    alert('Gender')
+    console.log('SelectGender>>',SelectGender)
+}
+function resetForm(){
+    document.formName.vale=""
+}
